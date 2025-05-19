aef7773a0a6e0d526a030f235aba6b74b1e36cca
AEH_PP25L_ANTONKRAVCHENKO_61376 <kanton905@gmail.com>
2025-05-19 16:18:03 +0200

    mojaTrzeciaAplikacja

--- src/mojaTrzeciaAplikacja.java
+++ src/mojaTrzeciaAplikacja.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-
+class mojaTrzeciaAplikacja{
 public class  mojaTrzeciaAplikacja  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
