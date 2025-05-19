8db6c2179226eafcab2951bce58ccaf5a5ca91db
AEH_PP25L_ANTONKRAVCHENKO_61376 <kanton905@gmail.com>
2025-05-19 15:21:47 +0200

    Lab2

---
+++ src/mojaDrugaAplikacja.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package pl.pp;
+import java.util.Scanner;
+public class mojaDrugaAplikacja {
+        public static void main(String[] args) {
+            double x = 10;
+            double y = 2;
+            Scanner scanner = new Scanner(System.in);
+            var result = x + y;
+            System.out.println("x + y =" + result);
+            result = x - y;
+            System.out.println("xy=" + result);
+            result = x*y;
+            System.out.println("xy=" + result);
+            result = x/y;
+            System.out.println("x/y = " + result);
+            result = x % y;
+            System.out.println("xy=" + result);
+            System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
+            double first=scanner.nextDouble(); //prośba o wpisanie wartości typu double
+            double second=scanner.nextDouble();
+            System.out.println("x+y=" + (first + second));
+        }
+}
